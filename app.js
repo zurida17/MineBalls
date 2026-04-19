4e69622e18407afdc28da14b85834e194d84f5ff
Искандер <iskander.lhamidulla11@gmail.com>
2026-04-19 12:55:21 +0300

    Set initial HP to 100 for all weapons

--- app.js
+++ app.js
@@ -3802,7 +3802,7 @@ if (id === "snowball") {
 
     setWeapon(weapon) {
       this.weapon = weapon;
-      this.baseMaxHp = Math.max(60, Math.round(100 * (Number.isFinite(weapon.hpMultiplier) ? weapon.hpMultiplier : 1)));
+      this.baseMaxHp = 100;
       this.maxHp = this.baseMaxHp;
       this.hp = this.maxHp;
       applyWeaponBalanceFrame(weapon);
